9700cc99a2bc2621593fbe66e570b073d34f23e7
mrogovets <mrogovets@ukr.net>
2021-06-17 20:54:10 +0300

    fixed floating form

--- venom-weblayout/js/main.js
+++ venom-weblayout/js/main.js
@@ -13,12 +13,25 @@ menuButtonClose.addEventListener("click", () => {
 const hideForm = document.querySelector(".hide-form");
 const orderTicket = document.querySelector(".order-ticket");
 const orderTrigger = document.querySelector(".order-trigger");
+const orderTicketForm = document.querySelector(".order-ticket__form");
+
+const heightForm = orderTicket.offsetHeight;
 
 setTimeout(() => {
-  const heightForm = orderTicket.offsetHeight;
   hideForm.style.bottom = -heightForm + "px";
 }, 1000);
 
 orderTrigger.addEventListener("click", () => {
   hideForm.classList.toggle("hide-form-active");
+});
+
+orderTicketForm.addEventListener("change", (event) => {
+  const target = event.target;
+
+  const label = target.labels[0];
+  if (label && target.value) {
+    label.classList.add("order-ticket__label-focus");
+  } else {
+    label.classList.remove("order-ticket__label-focus");
+  }
 });
